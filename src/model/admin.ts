4e76ff33732cde01eb5b33144547c3aa7efef6fb
NubieMe <fahmi.mellow@gmail.com>
2024-04-21 12:47:06 +0700

    update: minor changes

--- src/model/admin.ts
+++ src/model/admin.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const AdminSchema = new mongoose.Schema(
+const AdminSchema = new mongoose.Schema(
     {
         username: { type: String, required: true },
         password: { type: String, required: true },
